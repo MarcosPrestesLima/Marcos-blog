9f0c856bbb8dafe10cb7a645d094e9a1d00d2523
MarcosPrestesLima <lpsocram@gmail.com>
2024-02-25 17:20:45 -0300

    Finalizado o desafio corretamente, terminado o conteúdo dos small cards e trocado o assunto do blog por algo que me identifico mais

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "SUZUKI GSX-R 1000 a famosa SRAD",
+    "description": "Esta moto é incrível, ela tem 185cv de potência 172kg, isto é, pouco a mais de 1cv de potência por kg.",
+    "photoCover":"https://mcn-images.bauersecure.com/wp-images/4136/951x634/02-gsx-r1000.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "ADMISSÃO DE AR POSICIONADA NA FRENTE DA MOTO",
+    "description": "Faz com que a pressão do ar aumente, como o turbo, mas de acordo com a velocidade, e sem perda de potência causada pela pressão da exaustão de uma turbina.",
+    "photoCover":"https://2.bp.blogspot.com/_2bmNe4Zkaww/TK060sq9OgI/AAAAAAAAAuc/FiTGCgFLUN0/s1600/ra1.png"
+  },
+  {
+    "id":"3",
+    "title": "POR ESSA EU NÃO ESPERAVA",
+    "description": "O freio dessa moto é tão bom que impressionou na primeira vez que andei, eu sabia que essa moto era boa, mas não imaginava que era possível ser tão boa.",
+    "photoCover":"https://img.speedweek.com/i/2/2f0542d6d8424dd08856e6a644d64447.jpg"
+  },
+  {
+    "id":"4",
+    "title": "APENAS ADITIVADA E A MÉDIA DE CONSUMO É 7KM/L",
+    "description": "O gasto para se divertir é grande, a gasolina será o menor custo, porém esta é a superbike mais barata de se manter.",
+    "photoCover":"https://static.poder360.com.br/2022/03/memes-gasolina-848x477.png"
   }
 ]
